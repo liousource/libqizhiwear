17f7e2b78cd31353657e3a05169040f80d8f897a
Smile <yong_2012@qq.com>
2021-11-11 01:50:17 +0800

    更新祺智穿戴SDK及接口文档，实现账号绑定功能。

--- SDKDemo/app/src/main/java/com/liousdk/demo/MainActivity.java
+++ SDKDemo/app/src/main/java/com/liousdk/demo/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.truescend.gofit.pagers.start.splash.SplashActivity;
+import com.truescend.gofit.pagers.start.splash.BindAccountActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         btnEnter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SplashActivity.class);
+                Intent intent = new Intent(MainActivity.this, BindAccountActivity.class);
                 startActivity(intent);
             }
         });
